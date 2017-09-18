3878704c8734ac241dbec754e0649972a0535756
Nam Nguyen <nam040494@gmail.com>
2017-09-18 09:20:55 +0700

    login-logout-signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :active_relationships, class_name: Relationship.name,
@@ -7,4 +10,13 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: Relationship.name,
     foreign_key: :followed_id, dependent: :destroy
   has_many :follower, through: :passive_relationships, source: :follower
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true,
+    length: {maximum: Settings.validates.name.maximum}
+  validates :email, presence: true,
+    length: {maximum: Settings.validates.email.maximum},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.validates.password.minimum}, allow_nil: true
 end
